18c6328816d8e7ab3955ae90bf1c4557b2782b2f
Gael <gael.lopez22@uabc.edu.mx>
2024-10-05 21:37:42 -0700

    interfaz

--- Interfaz.java
+++ Interfaz.java
@@ -14,5 +14,27 @@ public class Interfaz {
     public void makeFrame()
     {
         frame = new JFrame("El mentiroso");
+        Container containerPane = frame.getContentPane();
+
+        JLabel tituloJuego = new JLabel("El mentiroso");
+        containerPane.add(tituloJuego, BorderLayout.NORTH);
+
+        //Botón para jugar
+        JButton botonJugar = new JButton("Jugar");
+        botonJugar.addActionListener(event -> botonJugar());
+
+        //Bóton para acerca de:
+        JButton botonAcerca = new JButton("Acerca de");
+        botonAcerca.addActionListener(event -> botonAcerca());
+    }
+
+    public void botonJugar()
+    {
+        System.out.println("Simon");
+    }
+
+    public void botonAcerca()
+    {
+        System.out.println("Simon2");
     }
 }
